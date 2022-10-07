af87089cd446d7440eeb9fc5f1dadc709bc2e530
ankaa-19 <107974233+ankaa-19@users.noreply.github.com>
2022-10-07 20:10:55 +0800

    update

---
+++ WarOfUs/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public float p1health;
+    public float p2health;
+    public int rand;
+    public float p2max;
+    public float p1max;
+
+    public void p1hp()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void p1hk()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void p1lp()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void p1lk()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void p1s()
+    {
+        SceneManager.LoadScene(6);
+    }
+}
